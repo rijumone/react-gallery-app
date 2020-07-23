43ecc8e93216f228ff26465309537a6a4359791f
Rijumone Choudhuri <rijumone@pop-os.localdomain>
2020-07-24 02:59:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Soon, this app will be a GalleryApp.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
